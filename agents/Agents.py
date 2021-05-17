6fe0421d82de22cd28eabd0b1e142cc5726f1d66
matiusz <matszwecja@gmail.com>
2021-05-17 11:22:39 +0200

    Simple emotional agents

--- agents/Agents.py
+++ agents/Agents.py
@@ -4,8 +4,98 @@ class AbstractAgent():
     def __init__(self, mc):
         self.total_reward = 0
         self.mc = mc
+    def reset(self):
+        pass
     def react(self, observation, reward):
         self.total_reward += reward
+
+class MimicAgent(AbstractAgent):
+    def __init__(self, mc):
+        super().__init__(mc)
+        self.lastObs = None
+        self.mode = "coop"
+    def reset(self):
+        self.mode = "coop"
+        self.lastObs = None
+    def react(self, observation, reward):
+        super().react(observation, reward)
+        min_obs = min(observation)
+        if self.lastObs is None:
+            self.lastObs = observation
+            return min_obs
+        else:
+            diffs = [observation[i] - self.lastObs[i] for i in range(len(observation))]
+            self.lastObs = observation
+            min_diffs = min(diffs)
+            if min_diffs < 0:
+                self.mode ="defect"
+            if min_diffs >= 0:
+                self.mode = "coop"
+            if self.mode == "defect":
+                return max(min_obs - (min_obs - self.mc) * 0.1, self.mc)
+            else:
+                return max(min_obs, self.mc)
+
+
+
+class SimpleRetalitatingAgent(AbstractAgent):
+    def __init__(self, mc):
+        super().__init__(mc)
+        self.lastObs = None
+        self.mode = "coop"
+    def reset(self):
+        self.mode = "coop"
+        self.lastObs = None
+    def react(self, observation, reward):
+        super().react(observation, reward)
+        min_obs = min(observation)
+        if self.lastObs is None:
+            self.lastObs = observation
+            return min_obs
+        else:
+            diffs = [observation[i] - self.lastObs[i] for i in range(len(observation))]
+            self.lastObs = observation
+            if self.mode == "defect":
+                return max(min_obs - (min_obs - self.mc) * 0.1, self.mc)
+            else:
+                if min(diffs)<0:
+                    self.mode = "defect"
+                    return max(min_obs - (min_obs - self.mc) * 0.1, self.mc)
+                else:
+                    return max(min_obs, self.mc)
+
+class SimpleForgivingAgent(AbstractAgent):
+    def __init__(self, mc):
+        super().__init__(mc)
+        self.lastObs = None
+        self.trustLevel = 2
+        self.mode = "coop"
+    def reset(self):
+        self.mode = "coop"
+        self.trustLevel = 2
+        self.lastObs = None
+    def react(self, observation, reward):
+        super().react(observation, reward)
+        min_obs = min(observation)
+        if self.lastObs is None:
+            self.lastObs = observation
+            return min_obs
+        else:
+            diffs = [observation[i] - self.lastObs[i] for i in range(len(observation))]
+            self.lastObs = observation
+            min_diffs = min(diffs)
+            if min_diffs < 0 and self.trustLevel > 0:
+                self.trustLevel -= 1
+            if min_diffs >= 0 and self.trustLevel < 2:
+                self.trustLevel += 1
+            if self.trustLevel == 0:
+                self.mode = "defect"
+            if self.trustLevel == 2:
+                self.mode = "coop"
+            if self.mode == "defect":
+                return max(min_obs - (min_obs - self.mc) * 0.1, self.mc)
+            else:
+                return max(min_obs, self.mc)
 
 class RandomAgent(AbstractAgent):
     def react(self, observation, reward):
@@ -15,26 +105,17 @@ class RandomAgent(AbstractAgent):
 class LoweringAgent(AbstractAgent):
     def react(self, observation, reward):
         super().react(observation, reward)
-        min_obs = 1
-        for obs in observation:
-            if obs<min_obs:
-                min_obs = obs
+        min_obs = min(observation)
         return max(min_obs - (min_obs - self.mc)*0.1, self.mc)
 1
 class KeepingAgent(AbstractAgent):
     def react(self, observation, reward):
         super().react(observation, reward)
-        min_obs = 1
-        for obs in observation:
-            if obs < min_obs:
-                min_obs = obs
+        min_obs = min(observation)
         return max(min_obs, self.mc)
 1
 class IncreasingAgent(AbstractAgent):
     def react(self, observation, reward):
         super().react(observation, reward)
-        min_obs = 1
-        for obs in observation:
-            if obs < min_obs:
-                min_obs = obs
+        min_obs = min(observation)
         return max(min_obs * 1.1, self.mc)
--- gym-bertrand/gym_bertrand/envs/bertrand_env.py
+++ gym-bertrand/gym_bertrand/envs/bertrand_env.py
@@ -1,50 +1,23 @@
 import gym
-import tensorflow as tf
-from gym import spaces
-from collections import namedtuple
-from tf_agents.specs import BoundedArraySpec
-from tf_agents.specs import ArraySpec
-from tf_agents.trajectories.time_step import TimeStep, StepType
-from tf_agents.environments import py_environment
-from tf_agents.environments import wrappers
+from gym import error, spaces, utils
 from gym.utils import seeding
 
-tf.compat.v1.enable_v2_behavior()
 
-class BertrandEnv(gym.Env, py_environment.PyEnvironment):
+class BertrandEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
     def __init__(self):
         super(BertrandEnv, self).__init__()
-        self.epoch = 0
-        self.agent_count = 0
-        self.marginal_cost = 0
-        self.reservation_price = 0
-        self.action_space = None
-        self.laststep = None
-        self.act_spec = None
-        self.obs_spec = None
 
     def custom_init(self, agent_count, marginal_cost, reservation_price):
         self.epoch = 0
         self.agent_count = agent_count
         self.marginal_cost = marginal_cost
-        self.reservation_price = reservation_price
+        self.reservation_price  = reservation_price
         self.action_space = spaces.Box(low=self.marginal_cost, high=1, shape=[])
         self.laststep = [1 for x in range(self.agent_count)]
-        min_obs = [[self.marginal_cost] * (
-                self.agent_count - 1)] * self.agent_count
-        max_obs = [[1.0] * (self.agent_count - 1)] * self.agent_count
-        self.act_spec = BoundedArraySpec((), float, 0.0, 1.0,
-                                            'action')
-        self.obs_spec = BoundedArraySpec(
-            (self.agent_count, self.agent_count - 1),
-            float, min_obs, max_obs, 'observation')
 
-    # def step(self, action):
-    #     return self._step(action)
-
-    def _step(self, action):
+    def step(self, action):
         bestOffer = None
         bestCompetitors = []
         for i in range(len(action)):
@@ -54,10 +27,9 @@ class BertrandEnv(gym.Env, py_environment.PyEnvironment):
             elif bestOffer == action[i]:
                 bestCompetitors.append(i)
         reward_n = [0 for x in range(len(action))]
-        if bestOffer <= self.reservation_price:
+        if bestOffer<=self.reservation_price:
             for comp in bestCompetitors:
-                reward_n[comp] = (bestOffer - self.marginal_cost) / len(
-                    bestCompetitors)
+                reward_n[comp] = (bestOffer - self.marginal_cost) / len(bestCompetitors)
 
         self.laststep = action
         obs_n = []
@@ -66,46 +38,18 @@ class BertrandEnv(gym.Env, py_environment.PyEnvironment):
 
         self.epoch += 1
 
-        done_n = (self.epoch >= 20)
+        done_n = (self.epoch>=20)
 
         info_n = {'n': []}
-        step_type = StepType.LAST if done_n else StepType.MID
+        return obs_n, reward_n, done_n, info_n
 
-        time_step = TimeStep(step_type, reward_n, 1.0, obs_n)
-
-        return time_step
-
-    def time_step_spec(self):
-        reward_spec = ArraySpec((self.agent_count, ), float, 'reward')
-        TimeStepSpec = namedtuple('TimeStepSpec', ['observation', 'reward'])
-
-        time_step_spec = TimeStepSpec(self.obs_spec, reward_spec)
-        return time_step_spec
-
-    def action_spec(self):
-        return self.act_spec
-
-    def observation_spec(self):
-        return self.obs_spec
-
-    # def reset(self):
-    #     return self._reset()
-
-    def _reset(self):
+    def reset(self):
         self.laststep = [0 for x in range(self.agent_count)]
         self.epoch = 0
-        reward = [0.0] * self.agent_count
-        observation = []
-        for i in range(self.agent_count):
-            observation.append(self.laststep[:i] + self.laststep[(i + 1):])
-        time_step = TimeStep(StepType.FIRST, reward, 1.0, observation)
-        return time_step
+        return self.laststep
 
     def render(self, mode='human'):
         print(self.laststep)
-
-    def _seed(self):
-        return self.seed()
 
     def close(self):
         pass
